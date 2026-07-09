31d8f2d241848ed5160d45ecc8385e933ec78832
jun <gaoj@cogent-technologies.net>
2026-07-10 07:40:08 +0800

    fix: 修复serverManager.lua脚本中一处变量为空导致oui-httpd崩溃得问题

--- applications/oui-app-server/files/rpc/serverManager.lua
+++ applications/oui-app-server/files/rpc/serverManager.lua
@@ -283,7 +283,10 @@ function M.getHostRtt()
     log.info('getHostRtt...')
 
     local server = M.getServerIP()
-    return ping(nil, server[1])
+    if not server then
+        return nil
+    end
+    return ping(nil, server)
 end
 
 -- 获取VPN隧道的连通性，比较耗时，需要在vue中异步获取
